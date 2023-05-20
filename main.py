7db325d0ebdff3812397f939a58591837db30ba1
DarkWolf890 <59552326+DarkWolf890@users.noreply.github.com>
2023-05-20 01:58:51 +0000

    added start menu

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from random import randint as r
 Item_type = ""
-Item_name = ""
+
+
+electronics = ["capacitor","Resistor","mcu","transistor","relay"]
+wiring = ["singlecore","multicore"]
+tools = []
+pchardware = ["fans","cpu","ram","hdd","ssd","psu","gpu","pccase"]
+documents = ["manuals","studies"]
+printer = []
+Item_types = [electronics,wiring,tools,pchardware,documents,printer]
+
+def display_menu():
+  ims_logo =  '''
+    _   _   _  
+ / \ / \ / \ 
+( I | M | S )
+ \_/ \_/ \_/ \n \n \n \n \n
+
+ Welcome to Inventory Management Software
+ What you want to do?
+'''
+print(img_logo)
+
+
+
 Item_quantity = 0
 
 
 
-class Item(item_type,item_name,item_quantity):
 
-    pass
+
+    
 
