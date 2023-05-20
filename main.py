29dc4c768f9c384d37d6399d7465eddec70bafc2
DarkWolf890 <59552326+DarkWolf890@users.noreply.github.com>
2023-05-20 02:06:23 +0000

    reconf

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Item_types = [electronics,wiring,tools,pchardware,documents,printer]
 def display_options(options):
   n = 1
   for option in options:
-    print(n,option + "\n \n \n")
+    print(n,option + "\n")
     n += 1
 
 def display_menu():
@@ -20,7 +20,7 @@ def display_menu():
   _   _   _  
  / \ / \ / \ 
 ( I | M | S )
- \_/ \_/ \_/  \n \n \n \n \n \n \n \n
+ \_/ \_/ \_/  \n \n \n \n \n 
 
  Welcome to Inventory Management Software
  What you want to do?\n \n \n
